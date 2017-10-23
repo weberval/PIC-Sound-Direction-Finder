0c979bf3d0d28429a53844ed2d88e38bf21ba41a
Valentin Weber <weberv@dhbw-loerrach.de>
2017-10-23 10:07:14 +0200

    Initial commit

---
+++ PIC-Sound-Direction-Finder.X/main.c
@@ -0,0 +1,29 @@
+/*
+-------------------------------------------------------------------
+File:		Main.c
+Title: 		PIC-Sound-Direction-Finder
+Hardware:	APIC or PICDem2+
+			Simulation also possible
+Controller:	PIC18F4520
+Frequency:	4 MHz (PLL disabled)
+Compiler:   XC8 ver. 1.38
+MPLAB X:    v4.01     
+Version:	0.0.0
+Date:		23.10.2017
+Author:		Valentin Weber & Jan Müllek
+-------------------------------------------------------------------
+Revision history
+
+ - V0.0.0: Created File; Initial Commit; Author: Valentin Weber
+
+-------------------------------------------------------------------
+Description: 
+
+-------------------------------------------------------------------
+*/
+
+#include "Definitions.h"
+
+void main(void) {
+    
+}
